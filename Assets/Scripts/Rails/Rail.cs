b2f0070f2b517b9b1306b25dd7629ec39405b058
wenright <wenright@purdue.edu>
2017-04-08 17:59:28 -0500

    Basic track movement

---
+++ Assets/Scripts/Rails/Rail.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rail : MonoBehaviour {
+
+	public Rail next;
+	public GameObject railObject;
+
+	void Start () {
+		gameObject.name = "rail";
+	}
+	
+	void Update () {
+		transform.Translate(Vector2.up * -RailSpawner.speed * Time.deltaTime);
+
+		if (next == null && transform.position.y <= RailSpawner.spawnPoint.y) {
+			GameObject railInstance = Instantiate(railObject, transform.position + new Vector3(0, 1.25f, 0), Quaternion.identity);
+			next = railInstance.GetComponent<Rail>();
+		}
+
+		if (transform.position.y <= RailSpawner.destroyPoint.y) {
+			Destroy(gameObject);
+		}
+	}
+
+	public bool IsHead () {
+		// Thead car won't have a next rail
+		return next == null;
+	}
+
+	public bool HasPath () {
+		if (next == null) {
+			return this is DeadEndRail;
+		}
+
+		return next.HasPath();
+	}
+}
---
+++ Assets/Scripts/Rails/RailSpawner.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RailSpawner : MonoBehaviour {
+
+	public static Vector3 spawnPoint = new Vector3(0, 5, 0);
+	public static Vector3 destroyPoint = new Vector3(0, -5, 0);
+    public static float speed = 0.5f;
+    private static float acceloration = 0.0f;
+
+    void Update () {
+        speed += acceloration * Time.deltaTime;
+    }
+}
