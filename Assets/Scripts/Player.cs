96f54d1f81044783e0000ba3b546936b5c5f17da
wenright <wenright@purdue.edu>
2017-04-11 21:03:58 -0500

    Add game over state

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
 	public Rail currentRail;
 	public bool willTakeRightBranch = false;
 	public bool willTakeLeftBranch = false;
+	public Text gameOverText;
+	public Text tapToRetryText;
 
 	private float lastPosX;
+
+	// TODO there should be a game controller that deals with gameOver states
+	public bool gameOver = false;
 
 	// Use this for initialization
 	void Start () {
@@ -17,14 +23,24 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (currentRail == null) {
+			// This shouldn't ever happen, but just in case it should be dealt with
+			return;
+		}
+
 		// Transfer player to next rail
 		if (currentRail.transform.position.y <= -0.3) {
 			Rail nextRail = currentRail.GetNext();
 
 			if (nextRail is DeadEndRail) {
-				// TODO gameover
-				Debug.Log("Game Over!");
-				RailSpawner.speed = 0.0f;
+				if (!gameOver) {
+					gameOver = true;
+					RailSpawner.speed = 0.0f;
+
+					// TODO tweening and non max white colors
+					gameOverText.color = Color.white;
+					tapToRetryText.color = Color.white;
+				}
 			} else {
 				Rail branchToTake = nextRail;
 
@@ -45,10 +61,14 @@ public class Player : MonoBehaviour {
 
 		// Rotate player so that they follow the path of the rail
 		float vx = lastPosX - transform.position.x;
-		float rotZ = -Mathf.Atan2(RailSpawner.speed * Time.deltaTime, vx);
-		float rotationOffset = 90.0f;
-		transform.rotation = Quaternion.Euler(0, 0, rotZ * Mathf.Rad2Deg - rotationOffset);
-		lastPosX = transform.position.x;
+		if (Mathf.Abs(vx) >= 0.000001f) {
+			float rotZ = -Mathf.Atan2(RailSpawner.speed * Time.deltaTime, vx);
+			float rotationOffset = 90.0f;
+			transform.rotation = Quaternion.Euler(0, 0, rotZ * Mathf.Rad2Deg - rotationOffset);
+			lastPosX = transform.position.x;
+		} else {
+			transform.rotation = Quaternion.identity;
+		}
 	}
 
 	// TODO Once 3 directional branches are added, swipe direction will become important
--- Assets/Scripts/Rails/RailSpawner.cs
+++ Assets/Scripts/Rails/RailSpawner.cs
@@ -6,49 +6,51 @@ public class RailSpawner : MonoBehaviour {
 
 	public static Vector3 spawnPoint = new Vector3(0, 5, 0);
 	public static Vector3 destroyPoint = new Vector3(0, -7, 0);
-    public static float speed = 3f;
-    public static GameObject rail;
-    public static GameObject branchRight;
-    public static GameObject branchLeft;
-    public static GameObject deadEnd;
+	public static float speed = 3f;
+	public static GameObject rail;
+	public static GameObject branchRight;
+	public static GameObject branchLeft;
+	public static GameObject deadEnd;
 
-    public UnityEngine.UI.Text scoreText;
-    public float score = 0.0f;
+	public UnityEngine.UI.Text scoreText;
+	public float score = 0.0f;
 
-    public static Player player;
+	public static Player player;
 
-    private static float acceleration = 0.1f;
+	private static float acceleration = 0.1f;
 
-    public static List<Rail> rails;
+	public static List<Rail> rails;
 
-    void Start () {
-    	// TODO remove this for actual game. Useful for debugging though
-        Random.InitState(321123);
+	void Start () {
+		// TODO remove this for actual game. Useful for debugging though
+		Random.InitState(321123);
 
-    	rails = new List<Rail>();
+		rails = new List<Rail>();
 
-    	GameObject playerObject = GameObject.FindWithTag("Player");
-    	player = playerObject.GetComponent<Player>();
+		GameObject playerObject = GameObject.FindWithTag("Player");
+		player = playerObject.GetComponent<Player>();
 
 		rail = Resources.Load("rail") as GameObject;
 		branchRight = Resources.Load("branchRight") as GameObject;
-        branchLeft = Resources.Load("branchLeft") as GameObject;
+		branchLeft = Resources.Load("branchLeft") as GameObject;
 		deadEnd = Resources.Load("deadEnd") as GameObject;
-    }
+	}
 
-    void Update () {
-    	score += (speed * Time.deltaTime) / 1.5f;
-    	scoreText.text = ((int) score).ToString();
+	void Update () {
+		if (!player.gameOver) {
+			score += (speed * Time.deltaTime) / 1.5f;
+			scoreText.text = ((int) score).ToString();
 
-        speed += acceleration * Time.deltaTime;
-    }
+			speed += acceleration * Time.deltaTime;
+		}
+	}
 
-    // Add rail to rails list
-    public static void AddRail (Rail obj) {
-    	rails.Add(obj);
-    }
+	// Add rail to rails list
+	public static void AddRail (Rail obj) {
+		rails.Add(obj);
+	}
 
-    public static void RemoveRail (Rail obj) {
-    	rails.Remove(obj);
-    }
+	public static void RemoveRail (Rail obj) {
+		rails.Remove(obj);
+	}
 }
